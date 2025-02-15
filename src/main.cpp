24fee6090d36bd48ffef9d0ac639ae40b9433016
Raki <824691+Raki@users.noreply.github.com>
2025-02-15 12:52:42 -0800

    Added vk_images.cpp and vk_images.h Added basic renderloop Added draw_background

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <vk_engine.h>
 
+
 int main(int argc, char* argv[])
 {
 	VulkanEngine engine;
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,11 +5,66 @@
 
 #include <vk_types.h>
 
+struct DeletionQueue
+{
+	std::deque<std::function<void()>> deletors;
+
+	void push_function(std::function<void()>&& function) {
+		deletors.push_back(function);
+	}
+
+	void flush() {
+		// reverse iterate the deletion queue to execute all the functions
+		for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+			(*it)(); //call functors
+		}
+
+		deletors.clear();
+	}
+};
+
+struct FrameData {
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+	VkSemaphore _swapchainSemaphore, _renderSemaphore;
+	VkFence _renderFence;
+	DeletionQueue _deletionQueue;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine {
 public:
 
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+	FrameData _frames[FRAME_OVERLAP];
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
+
 	bool _isInitialized{ false };
 	int _frameNumber {0};
+
+	DeletionQueue _mainDeletionQueue;
+
+	VmaAllocator _allocator;
+
+	//draw resources
+	AllocatedImage _drawImage;
+	VkExtent2D _drawExtent;
 
 	VkExtent2D _windowExtent{ 1700 , 900 };
 
@@ -26,4 +81,16 @@ public:
 
 	//run main loop
 	void run();
+
+	void draw_background(VkCommandBuffer cmd);
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+private:
+
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
